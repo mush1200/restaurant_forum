f314ba72fbfa224b195a8f78491cf27fea203027
mush1200 <mush1200@hotmail.com>
2021-08-27 10:17:55 +0800

    Add userController and signup page

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const db = require('./models') // 引入資料庫
 const app = express()
+const bodyParser = require('body-parser')
 const port = 3000
 
 app.engine('handlebars', handlebars({defaultLayout: 'main'})) // Handlebars 註冊樣板引擎
 app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
-
+app.use(bodyParser.urlencoded({extended: true}))
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
