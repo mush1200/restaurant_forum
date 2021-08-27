721bbe0582e4fc78db5fbb32407f204feb25a96c
mush1200 <mush1200@hotmail.com>
2021-08-27 10:23:02 +0800

    Confirm user password and unique email

--- app.js
+++ app.js
@@ -2,12 +2,25 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const db = require('./models') // 引入資料庫
 const app = express()
-const bodyParser = require('body-parser')
+
 const port = 3000
+
+const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
 
 app.engine('handlebars', handlebars({defaultLayout: 'main'})) // Handlebars 註冊樣板引擎
 app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+// 把 req.flash 放到 res.locals 裡面
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
