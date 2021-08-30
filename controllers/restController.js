cf2b908341e98cb10078ed73b1d3367ffba8feca
mush1200 <mush1200@hotmail.com>
2021-08-30 21:17:24 +0800

    Add pagination on restaurants index

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,32 +1,49 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
+const pageLimit = 10
 
 const restController = {
   getRestaurants: (req, res) => {
+    let offset = 0
     const whereQuery = {}
     let categoryId = ''
+     if (req.query.page) {
+      offset = (req.query.page - 1) * pageLimit
+    }
     if (req.query.categoryId) {
       categoryId = Number(req.query.categoryId)
       whereQuery.CategoryId = categoryId
-      console.log(req.query.categoryId)
     }
-    Restaurant.findAll({ include: Category, where: whereQuery }).then(restaurants => {
-      const data = restaurants.map(r => ({
-        ...r.dataValues,
-        description: r.dataValues.description.substring(0, 50),
-        categoryName: r.Category.name
-      }))
-      Category.findAll({
-        raw: true,
-        nest: true
-      }).then(categories => {
-        return res.render('restaurants', {
-          restaurants: data,
-          categories: categories,
-          categoryId: categoryId
+    Restaurant.findAndCountAll({ 
+      include: Category, 
+      where: whereQuery, 
+      offset: offset, 
+      limit: pageLimit }).then(result => {
+        const page = Number(req.query.page) || 1
+        const pages = Math.ceil(result.count / pageLimit)
+        const totalPage = Array.from({ length: pages }).map((item, index) => index + 1)
+        const prev = page - 1 < 1 ? 1 : page - 1
+        const next = page + 1 > pages ? pages : page + 1
+        const data = result.rows.map(r => ({
+          ...r.dataValues,
+          description: r.dataValues.description.substring(0, 50),
+          categoryName: r.dataValues.Category.name
+        }))
+        Category.findAll({
+          raw: true,
+          nest: true
+        }).then(categories => {
+          return res.render('restaurants', {
+            restaurants: data,
+            categories: categories,
+            categoryId: categoryId,
+            page: page,
+            totalPage: totalPage,
+            prev: prev,
+            next: next
+          })
         })
-      })
     })
   },
   getRestaurant: (req, res) => {
