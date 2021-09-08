e86a7b7ed9d3b34967bd40c359f19887a2043007
mush1200 <mush1200@hotmail.com>
2021-09-08 13:38:59 +0800

    add DELETE /api/admin/restaurants/:id

--- routes/apis.js
+++ routes/apis.js
@@ -10,5 +10,7 @@ router.get('/admin/restaurants', adminController.getRestaurants)
 router.get('/admin/restaurants/:id', adminController.getRestaurant)
 //後臺瀏覽全部類別
 router.get('/admin/categories', categoryController.getCategories)
+//後臺刪除一筆餐廳資料
+router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
 
 module.exports = router
